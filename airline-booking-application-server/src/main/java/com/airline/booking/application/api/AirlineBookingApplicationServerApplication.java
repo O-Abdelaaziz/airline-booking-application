fd4cbc25659edfbd8d2aaf0b3be8bda806578b1a
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2025-08-25 18:12:49 +0200

    📌 Task 1: first commit & scaffolding spring boot project

---
+++ airline-booking-application-server/src/main/java/com/airline/booking/application/api/AirlineBookingApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package com.airline.booking.application.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineBookingApplicationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineBookingApplicationServerApplication.class, args);
+    }
+
+}
---
+++ airline-booking-application-server/src/test/java/com/airline/booking/application/api/AirlineBookingApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airline.booking.application.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineBookingApplicationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
