56673b7bce8769555ca1446c6ac4bae17788291c
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2025-08-28 18:18:12 +0200

    📌 Task 19: implement find flight page

--- airline-booking-application-client/src/App.js
+++ airline-booking-application-client/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./components/common/Footer/Footer";
 import RegisterPage from "./components/authentication/RegisterPage/RegisterPage";
 import LoginPage from "./components/authentication/LoginPage/LoginPage";
 import HomePage from "./components/pages/HomePage/HomePage";
+import FindFlightsPage from "./components/pages/FindFlightsPage/FindFlightsPage";
 
 function App() {
   return (
@@ -13,11 +14,13 @@ function App() {
 
       <div className="content">
         <Routes>
+          {/* AUTHENTICATION PAGES */}
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
 
           {/* PUBLIC PAGES */}
           <Route path="/home" element={<HomePage />} />
+          <Route path="/flights" element={<FindFlightsPage />} />
 
           {/* Fallback for unmatched routes */}
           <Route path="*" element={<Navigate to="/home" />} />
